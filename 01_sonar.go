2514ad2cf2c81203b29756e5b869967dad922157
Mark Doyle <markdoyle222@gmail.com>
2021-12-20 02:59:10 +0000

    day one

---
+++ 01_sonar.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("data/sonar.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var prev int
+	var nIncreases int
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		val, err := strconv.Atoi(text)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if val > prev && prev != 0 {
+			nIncreases++
+		}
+
+		prev = val
+	}
+
+	fmt.Println(nIncreases)
+}
