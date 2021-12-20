5d035bea9f9a1a28b70c76a0a44f963eb2804e3e
Mark Doyle <markdoyle222@gmail.com>
2021-12-20 03:32:46 +0000

    day two

---
+++ 03_commands.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("data/commands.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var depth int
+	var x int
+
+	for scanner.Scan() {
+		vals := strings.Split(scanner.Text(), " ")
+		command := vals[0]
+		val := vals[1]
+
+		ival, err := strconv.Atoi(val)
+
+		if err != nil {
+			panic(err)
+		}
+
+		switch command {
+		case "forward":
+			x = x + ival
+		case "down":
+			depth = depth + ival
+		case "up":
+			depth = depth - ival
+		}
+	}
+
+	fmt.Println(depth * x)
+}
