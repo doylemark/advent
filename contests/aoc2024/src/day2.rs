e04ebc9b0ab9f1f09bf9b6450cc324729d314037
Mark Doyle <mark@mdoyle.sh>
2024-12-04 23:32:49 +0000

    init fsm

--- contests/aoc2024/src/day2.rs
+++ contests/aoc2024/src/day2.rs
@@ -4,56 +4,68 @@ use crate::*;
 
 impl Day2 for Year2024 {
     fn part1(input: String) -> impl Display {
-        let mut sum = 0;
+        input
+            .lines()
+            .filter_map(|line| {
+                let nums = line
+                    .trim()
+                    .split_whitespace()
+                    .map(|n| n.parse::<i32>().unwrap())
+                    .collect::<Vec<_>>();
 
-        for line in input.lines() {
-            let nums = line
-                .trim()
-                .split_whitespace()
-                .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<_>>();
-
-            if is_valid_line(&nums) {
-                sum += 1;
-            }
-        }
-
-        sum
+                nums.windows(2)
+                    .all(|w| w[0].cmp(&w[1]) == nums[0].cmp(&nums[1]) && w[0].abs_diff(w[1]) <= 3)
+                    .then_some(())
+            })
+            .count()
     }
 
-    // n^2 :/
     fn part2(input: String) -> impl Display {
         let mut sum = 0;
 
         for line in input.lines() {
-            let nums = line
+            let mut nums = line
                 .trim()
                 .split_whitespace()
-                .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<_>>();
-
-            let input = nums.clone();
-
-            if is_valid_line(&nums) {
-                sum += 1;
-            } else {
-                for i in 0..input.len() {
-                    let mut removed = input.clone();
-                    removed.remove(i);
-
-                    if is_valid_line(&removed) {
-                        sum += 1;
-                        break;
-                    }
-                }
-            }
+                .map(|n| n.parse::<i32>().unwrap());
         }
 
         sum
     }
 }
 
-fn is_valid_line(nums: &Vec<i32>) -> bool {
-    nums.windows(2)
-        .all(|w| w[0].cmp(&w[1]) == nums[0].cmp(&nums[1]) && w[0].abs_diff(w[1]) <= 3)
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum State {
+    Idle,
+    Terminated,
+    Asc,
+    Desc,
+}
+
+#[derive(Debug)]
+struct FSM {
+    inner: State,
+    transitions: i32,
+}
+
+impl FSM {
+    fn new() -> Self {
+        Self {
+            inner: State::Idle,
+            transitions: 0,
+        }
+    }
+
+    fn state(&self) -> &State {
+        &self.inner
+    }
+
+    fn transition(&mut self, new: State) {
+        println!("new=[{new:?}]");
+        self.inner = new;
+    }
+
+    fn next(&mut self, prev: i32, n: i32) {
+        let delta = prev - n;
+    }
 }
