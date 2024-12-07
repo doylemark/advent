b78536676efb0ce8a12ba81567f2169c88e671fa
Mark Doyle <mark@mdoyle.sh>
2024-12-07 23:41:09 +0000

    tidy

--- contests/aoc2024/src/day7.rs
+++ contests/aoc2024/src/day7.rs
@@ -1,45 +1,34 @@
 use crate::*;
 use std::fmt::Display;
 
+fn parse_input(input: &str) -> impl Iterator<Item = (i64, Vec<i64>)> + '_ {
+    input.lines().map(|l| {
+        let (target, nums) = l.split_once(":").unwrap();
+
+        let target = target.parse::<i64>().unwrap();
+
+        let nums = nums
+            .split_whitespace()
+            .map(|num| num.parse::<i64>().unwrap())
+            .collect::<Vec<_>>();
+
+        (target, nums)
+    })
+}
+
 impl Day7 for Year2024 {
     fn part1(input: String) -> impl Display {
-        let mut sum = 0;
-
-        for line in input.lines() {
-            let (target, nums) = line.split_once(":").unwrap();
-
-            let target = target.parse::<i64>().unwrap();
-
-            let nums = nums
-                .split_whitespace()
-                .map(|num| num.parse::<i64>().unwrap())
-                .collect::<Vec<_>>();
-
-            if try_all_combinations(&nums, &vec!['+', '*'], target) {
-                sum += target;
-            }
-        }
-        sum
+        let ops = vec!['+', '*'];
+        parse_input(&input)
+            .map(|(target, nums)| try_all_combinations(&nums, &ops, target))
+            .sum::<i64>()
     }
 
     fn part2(input: String) -> impl Display {
-        let mut sum = 0;
-
-        for line in input.lines() {
-            let (target, nums) = line.split_once(":").unwrap();
-
-            let target = target.parse::<i64>().unwrap();
-
-            let nums = nums
-                .split_whitespace()
-                .map(|num| num.parse::<i64>().unwrap())
-                .collect::<Vec<_>>();
-
-            if try_all_combinations(&nums, &vec!['+', '*', '|'], target) {
-                sum += target;
-            }
-        }
-        sum
+        let ops = vec!['+', '*', '|'];
+        parse_input(&input)
+            .map(|(target, nums)| try_all_combinations(&nums, &ops, target))
+            .sum::<i64>()
     }
 }
 
@@ -62,7 +51,7 @@ fn check_combination(nums: &Vec<i64>, target: i64, ops: &Vec<char>) -> bool {
     result == target
 }
 
-fn try_all_combinations(nums: &Vec<i64>, ops: &Vec<char>, target: i64) -> bool {
+fn try_all_combinations(nums: &Vec<i64>, ops: &Vec<char>, target: i64) -> i64 {
     let n = nums.len() - 1;
     let total_combinations = ops.len().pow(n as u32);
 
@@ -72,12 +61,13 @@ fn try_all_combinations(nums: &Vec<i64>, ops: &Vec<char>, target: i64) -> bool {
 
         for _ in 0..n {
             cur.push(ops[num % ops.len()]);
-            num /= 3
+
+            num /= ops.len()
         }
 
         if check_combination(nums, target, &cur) {
-            return true;
+            return target;
         }
     }
-    false
+    0
 }
