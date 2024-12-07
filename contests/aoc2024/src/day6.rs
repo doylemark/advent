fe9d17ba4118fefef00c7dfa5da3858519c6940a
Mark Doyle <mark@mdoyle.sh>
2024-12-07 03:24:36 +0000

    brute force p2

--- contests/aoc2024/src/day6.rs
+++ contests/aoc2024/src/day6.rs
@@ -54,44 +54,53 @@ impl Day6 for Year2024 {
     }
 
     fn part2(input: String) -> impl Display {
-        let (mut matrix, mut cur) = parse_input(&input);
-
-        let mut seen = HashSet::new();
-        let mut n = 1;
-        let mut dir = 0;
-
+        let (mut matrix, start) = parse_input(&input);
+        let mut n = 0;
         let dirs: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
         for i in 0..matrix.len() {
             for j in 0..matrix[i].len() {
-                if matrix[i][j] == '.' {
-                    matrix[i][j] = '#';
-                    if loop {
-                        let hash = (cur.0 * matrix.len() + cur.1) * dir + 4;
-
-                        if seen.contains(&hash) {
-                            break true;
-                        }
-                        seen.insert(hash);
-
-                        let di = cur.0.wrapping_add_signed(dirs[dir].0);
-                        let dj = cur.1.wrapping_add_signed(dirs[dir].1);
-
-                        let ch = match matrix.get(di).map(|r| r.get(dj)).flatten() {
-                            Some(ch) => ch,
-                            None => break false,
-                        };
-
-                        if '#' == *ch {
-                            dir = (dir + 1) % 4;
-                        } else {
-                            cur = (di, dj)
-                        }
-                    } {
-                        n += 1;
-                    };
-                    matrix[i][j] = '.';
+                if (i, j) == start || matrix[i][j] != '.' {
+                    continue;
                 }
+                println!("checking {},{}", i, j);
+
+                matrix[i][j] = '#';
+
+                let mut cur = start;
+                let mut dir = 0;
+                let mut seen = HashSet::new();
+
+                let mut turn = 0;
+
+                while let Some(Some(ch)) = matrix
+                    .get(cur.0.wrapping_add_signed(dirs[dir].0))
+                    .map(|r| r.get(cur.1.wrapping_add_signed(dirs[dir].1)))
+                {
+                    turn += 1;
+                    if turn > 100000 {
+                        break;
+                    }
+
+                    let hash = (cur.0 * matrix.len() + cur.1) * 4 + dir;
+
+                    if seen.contains(&hash) {
+                        n += 1;
+                        break;
+                    }
+                    seen.insert(hash);
+
+                    if *ch == '#' {
+                        dir = (dir + 1) % 4;
+                    } else {
+                        cur = (
+                            cur.0.wrapping_add_signed(dirs[dir].0),
+                            cur.1.wrapping_add_signed(dirs[dir].1),
+                        );
+                    }
+                }
+
+                matrix[i][j] = '.';
             }
         }
 
