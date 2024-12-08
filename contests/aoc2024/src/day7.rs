316af8f492190003133ead467ea256da05858d02
Mark Doyle <mark@mdoyle.sh>
2024-12-08 00:31:53 +0000

    make p1 a lot faster

--- contests/aoc2024/src/day7.rs
+++ contests/aoc2024/src/day7.rs
@@ -18,9 +18,8 @@ fn parse_input(input: &str) -> impl Iterator<Item = (i64, Vec<i64>)> + '_ {
 
 impl Day7 for Year2024 {
     fn part1(input: String) -> impl Display {
-        let ops = vec!['+', '*'];
         parse_input(&input)
-            .map(|(target, nums)| try_all_combinations(&nums, &ops, target))
+            .map(|(target, nums)| try_all_combinations_1(&nums, target))
             .sum::<i64>()
     }
 
@@ -32,25 +31,27 @@ impl Day7 for Year2024 {
     }
 }
 
-fn check_combination(nums: &Vec<i64>, target: i64, ops: &Vec<char>) -> bool {
-    let mut result = nums[0];
+fn try_all_combinations_1(nums: &[i64], target: i64) -> i64 {
+    let n = nums.len() - 1;
+    let total_combinations = 1 << n;
 
-    for i in 0..ops.len() {
-        match ops[i] {
-            '+' => result += nums[i + 1],
-            '*' => result *= nums[i + 1],
-            '|' => {
-                result = (result.to_string() + &nums[i + 1].to_string())
-                    .parse()
-                    .unwrap()
+    for i in 0..total_combinations {
+        let mut result = nums[0];
+
+        for pos in 0..n {
+            if i & (1 << pos) != 0 {
+                result += nums[pos + 1];
+            } else {
+                result *= nums[pos + 1];
             }
-            _ => unreachable!(),
+        }
+
+        if result == target {
+            return target;
         }
     }
-
-    result == target
+    0
 }
-
 fn try_all_combinations(nums: &Vec<i64>, ops: &Vec<char>, target: i64) -> i64 {
     let n = nums.len() - 1;
     let total_combinations = ops.len().pow(n as u32);
@@ -70,4 +71,22 @@ fn try_all_combinations(nums: &Vec<i64>, ops: &Vec<char>, target: i64) -> i64 {
         }
     }
     0
+}
+fn check_combination(nums: &Vec<i64>, target: i64, ops: &Vec<char>) -> bool {
+    let mut result = nums[0];
+
+    for i in 0..ops.len() {
+        match ops[i] {
+            '+' => result += nums[i + 1],
+            '*' => result *= nums[i + 1],
+            '|' => {
+                result = (result.to_string() + &nums[i + 1].to_string())
+                    .parse()
+                    .unwrap()
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    result == target
 }
